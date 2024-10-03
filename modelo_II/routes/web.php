3c78b3cbc689dfca5e09f57a5566f4f62d7a79fe
Abrahão Eneias <abrahao.eneias@dce.ufpb.br>
2024-10-03 16:29:33 -0300

    refactoring

--- modelo_II/routes/web.php
+++ modelo_II/routes/web.php
@@ -12,24 +12,11 @@ function renderView($view, $data = [])
 // Pega a rota atual (URL)
 $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 
-// Define as rotas da aplicação
-switch ($uri) {
-    case '/':
-        renderView('login');
-        break;
-    case '/login':
-        renderView('login');
-        break;
-    case '/home':
-        // Exibe o painel do usuário, falta implementar verificar se o usuário está autenticado
-        renderView('home');
-        break;
-    case '/logout':
-        // Processa o logout (destrói sessão, remove token, etc.)
-        require_once __DIR__ . '/../public/logout.php';
-        break;
-    default:
-        http_response_code(404);
-        echo 'Página não encontrada';
-        break;
-}
+// Define as rotas da aplicação usando match
+match ($uri) {
+    '/' => renderView('login'),
+    '/login' => renderView('login'),
+    '/home' => renderView('home'), // Falta implementar verificação de autenticação
+    '/logout' => require_once __DIR__ . '/../public/logout.php',
+    default => (http_response_code(404) && print ('Página não encontrada')),
+};
